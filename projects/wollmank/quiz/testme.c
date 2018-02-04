7797cd27cae10c48af4b2e5a2c77d6bcb8d5b43f
Kyle Wollman <wollmank@flip3.engr.oregonstate.edu>
2018-02-03 18:40:49 -0800

    random-quiz-submission

---
+++ projects/wollmank/quiz/testme.c
@@ -0,0 +1,73 @@
+/************************************************************************
+* Kyle Wollman
+* CS362 - 400 - W2018
+* random-quiz
+*************************************************************************/
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<time.h>
+
+char inputChar()
+{
+	char symbols[] = "[({ ax})]";
+	int i = random() % 9;
+	char c = symbols[i];
+    return c;
+}
+
+char* inputString()
+{
+	char letters[] = "rest\0";
+	int i, j;
+	char s[5];
+	
+	for(i=0; i < 6; i++){
+		j = random() % 5;
+		s[i] = letters[j];
+	}
+	
+   return s;
+}
+
+void testme()
+{
+  int tcCount = 0;
+  char *s;
+  char c;
+  int state = 0;
+  while (1)
+  {
+    tcCount++;
+    c = inputChar();
+    s = inputString();
+    printf("Iteration %d: c = %c, s = %s, state = %d\n", tcCount, c, s, state);
+
+    if (c == '[' && state == 0) state = 1;
+    if (c == '(' && state == 1) state = 2;
+    if (c == '{' && state == 2) state = 3;
+    if (c == ' '&& state == 3) state = 4;
+    if (c == 'a' && state == 4) state = 5;
+    if (c == 'x' && state == 5) state = 6;
+    if (c == '}' && state == 6) state = 7;
+    if (c == ')' && state == 7) state = 8;
+    if (c == ']' && state == 8) state = 9;
+    if (s[0] == 'r' && s[1] == 'e'
+       && s[2] == 's' && s[3] == 'e'
+       && s[4] == 't' && s[5] == '\0'
+       && state == 9)
+    {
+      printf("error ");
+      exit(200);
+    }
+  }
+}
+
+
+int main(int argc, char *argv[])
+{
+    srand(time(NULL));
+    testme();
+    return 0;
+}
