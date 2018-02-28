7f1d1ce7a5a685e08e7a385c9f773a28cc9a45cc
Kyle Wollman <wollmank@flip3.engr.oregonstate.edu>
2018-02-27 23:35:37 -0800

    Assignment 5 debugging

--- projects/wollmank/dominion/dominion.c
+++ projects/wollmank/dominion/dominion.c
@@ -399,7 +399,7 @@ int isGameOver(struct gameState *state) {
 
   //if three supply pile are at 0, the game ends
   j = 0;
-  for (i = 0; i < 25; i++)
+  for (i = 0; i < 27; i++)
     {
       if (state->supplyCount[i] == 0)
 	{
